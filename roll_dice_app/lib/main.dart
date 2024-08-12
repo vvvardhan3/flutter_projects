a747aec6e2529b2561286fc48b917efb968f5f25
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-12 20:52:23 +0530

    Flutter app

---
+++ roll_dice_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 52, 25, 99),
+                Color.fromARGB(255, 143, 98, 222)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
