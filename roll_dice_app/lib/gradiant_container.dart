e20b7e219a279e2ec1baf671596441fea1cd4081
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-12 21:17:04 +0530

    Created a new custom reusable widget

---
+++ roll_dice_app/lib/gradiant_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 52, 25, 99),
+            Color.fromARGB(255, 143, 98, 222)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradiant_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 52, 25, 99),
-                Color.fromARGB(255, 143, 98, 222)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
